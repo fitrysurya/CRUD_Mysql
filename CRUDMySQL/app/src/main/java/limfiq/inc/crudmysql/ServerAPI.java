f04407c02aeb68468d404aef121d1316943432d3
fitrysurya <fitrysuryaning011@gmail.com>
2019-11-19 10:01:25 +0700

    uts

---
+++ CRUDMySQL/app/src/main/java/limfiq/inc/crudmysql/ServerAPI.java
@@ -0,0 +1,8 @@
+package limfiq.inc.crudmysql;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.43.153/uts_mobile2/view_data.php";
+    public static final String URL_INSERT = "http://192.168.43.153/uts_mobile2/create_data.php";
+    public static final String URL_DELETE = "http://192.168.43.153/uts_mobile2/delete_data.php";
+    public static final String URL_UPDATE = "http://192.168.43.153/uts_mobile2/update_data.php";
+}
